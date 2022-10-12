187627314348b61fcdcdef21f9996f405d547f39
Антон Шадаев <shadaev2001@icloud.com>
2022-10-12 22:27:42 +0400

    Save logic added, buttons are renamed

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.util.SparseBooleanArray;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,38 +11,40 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView lv;
-    EditText et;
+    ArrayList<String> listOfBosses;
+    ListView listViewOfBosses;
+    EditText editTextBossFullName;
+
     Button buttonAdd;
-    Button button1;
-    Button button2;
-    Button button3;
+    Button buttonSelectAll;
+    Button buttonDeselectAll;
+    Button buttonPrintAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et = findViewById(R.id.editTextTextPersonName);
         buttonAdd = findViewById(R.id.buttonAdd);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        lv = (ListView) findViewById(R.id.lv); // получили доступ к listview
-        List<String> list = new ArrayList<>();
+        buttonSelectAll = findViewById(R.id.button1);
+        buttonDeselectAll = findViewById(R.id.button2);
+        buttonPrintAll = findViewById(R.id.button3);
+
+        listOfBosses = new ArrayList<>();
+        listViewOfBosses = findViewById(R.id.lv); // получили доступ к listview
+        editTextBossFullName = findViewById(R.id.editTextBossFullName);
 
 
         buttonAdd.setOnClickListener(view -> {
             try {
-                if (!list.contains(et.getText().toString())) {
-                    list.add(et.getText().toString());
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-                            R.layout.forlist, list); //то как данные будут храниться
-                    lv.setAdapter(adapter);
-                    lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                if (!listOfBosses.contains(editTextBossFullName.getText().toString())) {
+                    listOfBosses.add(editTextBossFullName.getText().toString());
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                            R.layout.forlist, listOfBosses); // то, как данные будут храниться
+                    listViewOfBosses.setAdapter(adapter);
+                    listViewOfBosses.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
                 } else {
                     Toast.makeText(MainActivity.this, "The element already exists!",
                             Toast.LENGTH_LONG).show();
@@ -56,12 +56,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        button1.setOnClickListener(view -> {
+        buttonSelectAll.setOnClickListener(view -> {
             try {
-                if (!list.isEmpty()) {
-                    for (int i = 0; i < list.size(); i++) {
-                        if (!list.get(i).isEmpty()) {
-                            lv.setItemChecked(i, true);
+                if (!listOfBosses.isEmpty()) {
+                    for (int i = 0; i < listOfBosses.size(); i++) {
+                        if (!listOfBosses.get(i).isEmpty()) {
+                            listViewOfBosses.setItemChecked(i, true);
                         }
                     }
                 } else {
@@ -74,12 +74,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button2.setOnClickListener(view -> {
+        buttonDeselectAll.setOnClickListener(view -> {
             try {
-                if (!list.isEmpty()) {
-                    for (int i = 0; i < list.size(); i++) {
-                        if (!list.get(i).isEmpty()) {
-                            lv.setItemChecked(i, false);
+                if (!listOfBosses.isEmpty()) {
+                    for (int i = 0; i < listOfBosses.size(); i++) {
+                        if (!listOfBosses.get(i).isEmpty()) {
+                            listViewOfBosses.setItemChecked(i, false);
                         }
                     }
                 } else {
@@ -92,30 +92,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button3.setOnClickListener(view -> {
+        buttonPrintAll.setOnClickListener(view -> {
             String result;
-            SparseBooleanArray sbArray = lv.getCheckedItemPositions();
+            SparseBooleanArray sbArray = listViewOfBosses.getCheckedItemPositions();
             for (int i = 0; i < sbArray.size(); i++) {
                 int key = sbArray.keyAt(i);
                 if (sbArray.get(key)) {
-                    result= list.get(i);
+                    result= listOfBosses.get(i);
                     Toast.makeText(MainActivity.this, result,
                             Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-
-    }
-
-    private List<String> initList() {
-        List<String> list = new ArrayList<>();
-
-        list.add("iPhone");
-        list.add("HTC");
-        list.add("Samsung");
-        list.add("LG");
-
-        return list;
     }
 }

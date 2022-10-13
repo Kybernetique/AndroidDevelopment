d4766a01b61c9e86c8b4954bc155531817fa6a79
Антон Шадаев <shadaev2001@icloud.com>
2022-10-13 16:48:39 +0400

    Created all fragments.

---
+++ app/src/main/java/com/example/lab1/FragmentCreate.java
@@ -0,0 +1,64 @@
+package com.example.lab1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class FragmentCreate extends Fragment {
+    ArrayList<String> listOfBosses;
+    ListView listViewOfBosses;
+    EditText editTextBossFullName;
+    Button buttonAdd;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+
+/*    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editTextBossFullName = view.findViewById(R.id.editTextBossFullName);
+        listViewOfBosses = view.findViewById(R.id.listViewOfBosses);
+        buttonAdd = view.findViewById(R.id.buttonAdd);
+    }*/
+
+/*    protected void addBoss() {
+        try {
+            if (!listOfBosses.contains(editTextBossFullName.getText().toString())) {
+                listOfBosses.add(editTextBossFullName.getText().toString());
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                        R.layout.forlist, listOfBosses); // то, как данные будут храниться
+                listViewOfBosses.setAdapter(adapter);
+                listViewOfBosses.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+            } else {
+                Toast.makeText(getActivity(), "The element already exists!",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception ex) {
+            Toast.makeText(getActivity(), "Error!",
+                    Toast.LENGTH_LONG).show();
+        }
+    }*/
+}
---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> listOfBosses;
+    ListView listViewOfBosses;
+    EditText editTextBossFullName;
+
+    Button buttonCreate;
+    Button buttonRead;
+    Button buttonUpdate;
+    Button buttonDelete;
+
+    Button buttonSelectAll;
+    Button buttonDeselectAll;
+    Button buttonPrintAll;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonCreate = findViewById(R.id.buttonCreate);
+        buttonRead = findViewById(R.id.buttonRead);
+        buttonUpdate = findViewById(R.id.buttonUpdate);
+        buttonDelete = findViewById(R.id.buttonDelete);
+
+        buttonCreate.setOnClickListener(view -> {
+            FragmentCreate fragmentCreate = new FragmentCreate();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                    .beginTransaction();
+            fragmentTransaction.replace(R.id.FragmentContainer, fragmentCreate);
+            fragmentTransaction.commit();
+/*            try {
+                if (!listOfBosses.contains(editTextBossFullName.getText().toString())) {
+                    listOfBosses.add(editTextBossFullName.getText().toString());
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                            R.layout.forlist, listOfBosses); // то, как данные будут храниться
+                    listViewOfBosses.setAdapter(adapter);
+                    listViewOfBosses.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                } else {
+                    Toast.makeText(MainActivity.this, "The element already exists!",
+                            Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error!",
+                        Toast.LENGTH_LONG).show();
+            }*/
+        });
+
+        buttonRead.setOnClickListener(view -> {
+            FragmentRead fragmentRead = new FragmentRead();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                    .beginTransaction();
+            fragmentTransaction.replace(R.id.FragmentContainer, fragmentRead);
+            fragmentTransaction.commit();
+        });
+
+        buttonUpdate.setOnClickListener(view -> {
+            FragmentUpdate fragmentUpdate = new FragmentUpdate();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                    .beginTransaction();
+            fragmentTransaction.replace(R.id.FragmentContainer, fragmentUpdate);
+            fragmentTransaction.commit();
+        });
+
+        buttonDelete.setOnClickListener(view -> {
+            FragmentDelete fragmentDelete = new FragmentDelete();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                    .beginTransaction();
+            fragmentTransaction.replace(R.id.FragmentContainer, fragmentDelete);
+            fragmentTransaction.commit();
+        });
+
+/*        buttonAdd = findViewById(R.id.buttonCreate);
+        buttonSelectAll = findViewById(R.id.buttonSelectAll);
+        buttonDeselectAll = findViewById(R.id.buttonDeselectAll);
+        buttonPrintAll = findViewById(R.id.buttonPrintAll);
+
+        listOfBosses = new ArrayList<>();
+        listViewOfBosses = findViewById(R.id.lv); // получили доступ к listview
+        editTextBossFullName = findViewById(R.id.editTextBossFullName);*/
+
+/*        buttonAdd.setOnClickListener(view -> {
+            try {
+                if (!listOfBosses.contains(editTextBossFullName.getText().toString())) {
+                    listOfBosses.add(editTextBossFullName.getText().toString());
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                            R.layout.forlist, listOfBosses); // то, как данные будут храниться
+                    listViewOfBosses.setAdapter(adapter);
+                    listViewOfBosses.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                } else {
+                    Toast.makeText(MainActivity.this, "The element already exists!",
+                            Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });*/
+
+
+/*        buttonSelectAll.setOnClickListener(view -> {
+            try {
+                if (!listOfBosses.isEmpty()) {
+                    for (int i = 0; i < listOfBosses.size(); i++) {
+                        if (!listOfBosses.get(i).isEmpty()) {
+                            listViewOfBosses.setItemChecked(i, true);
+                        }
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "List is empty!",
+                            Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });*/
+
+/*        buttonDeselectAll.setOnClickListener(view -> {
+            try {
+                if (!listOfBosses.isEmpty()) {
+                    for (int i = 0; i < listOfBosses.size(); i++) {
+                        if (!listOfBosses.get(i).isEmpty()) {
+                            listViewOfBosses.setItemChecked(i, false);
+                        }
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "List is empty!",
+                            Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });*/
+
+/*
+        buttonPrintAll.setOnClickListener(view -> {
+            String result;
+            SparseBooleanArray sbArray = listViewOfBosses.getCheckedItemPositions();
+            for (int i = 0; i < sbArray.size(); i++) {
+                int key = sbArray.keyAt(i);
+                if (sbArray.get(key)) {
+                    result = listOfBosses.get(i);
+                    Toast.makeText(MainActivity.this, result,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });*/
+    }
+}

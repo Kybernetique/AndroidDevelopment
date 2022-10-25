95f4e6b8917dcf38fd28f387a5927e887d47e018
Антон Шадаев <shadaev2001@icloud.com>
2022-10-26 01:10:52 +0400

    Горизонтальная ориентация работает

--- app/src/main/java/com/example/lab1/FragmentCRUD.java
+++ app/src/main/java/com/example/lab1/FragmentCRUD.java
@@ -1,9 +1,13 @@
 package com.example.lab1;
 
+import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.SparseBooleanArray;
@@ -14,6 +18,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,17 +27,12 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class FragmentCRUD extends Fragment {
-    ArrayAdapter<String> adapter;
+    FrameLayout frameLayout;
 
-    ArrayList<String> listOfBosses;
-    ListView listViewOfBosses;
+    View view;
 
-    EditText editTextBossFullName;
-
-    Button buttonCreate;
-    Button buttonRead;
-    Button buttonUpdate;
-    Button buttonDelete;
+    ArrayList<String> listOfBosses = new ArrayList<>();
+    ArrayAdapter adapter; // то, как данные будут храниться
 
     int selectedItemPosition;
     String selectedItemValue;
@@ -44,19 +45,18 @@ public class FragmentCRUD extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_crud, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        frameLayout = new FrameLayout(getActivity());
+        inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.fragment_crud, container, false);
 
-        listOfBosses = new ArrayList<>();
-        listViewOfBosses = view.findViewById(R.id.listViewOfBosses); // получили доступ к listview
-        editTextBossFullName = view.findViewById(R.id.editTextBossFullName);
+        ListView listViewOfBosses = view.findViewById(R.id.listViewOfBosses); // получили доступ к listview
+        EditText editTextBossFullName = view.findViewById(R.id.editTextBossFullName);
 
-        buttonCreate = view.findViewById(R.id.buttonCreate);
-        buttonRead = view.findViewById(R.id.buttonRead);
-        buttonUpdate = view.findViewById(R.id.buttonUpdate);
-        buttonDelete = view.findViewById(R.id.buttonDelete);
-
+        Button buttonCreate = view.findViewById(R.id.buttonCreate);
+        Button buttonRead = view.findViewById(R.id.buttonRead);
+        Button buttonUpdate = view.findViewById(R.id.buttonUpdate);
+        Button buttonDelete = view.findViewById(R.id.buttonDelete);
 
         listViewOfBosses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -66,26 +66,23 @@ public class FragmentCRUD extends Fragment {
             }
         });
 
-
         buttonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     if (!listOfBosses.contains(editTextBossFullName.getText().toString())) {
                         listOfBosses.add(editTextBossFullName.getText().toString());
-
-                        adapter = new ArrayAdapter<>(getActivity(),
-                                R.layout.forlist, listOfBosses); // то, как данные будут храниться
+                        adapter = new ArrayAdapter<>(getActivity(), R.layout.forlist, listOfBosses);
                         listViewOfBosses.setAdapter(adapter);
-                        listViewOfBosses.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                        listViewOfBosses.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
                         listViewOfBosses.clearChoices();
-                    } else {
-                        Toast.makeText(getActivity(), "The element already exists!",
-                                Toast.LENGTH_LONG).show();
                     }
-                } catch (Exception ex) {
-                    Toast.makeText(getActivity(), "Error!",
-                            Toast.LENGTH_LONG).show();
+                    else {
+                        Toast.makeText(getActivity(), "The element already exists!", Toast.LENGTH_LONG).show();
+                    }
+                }
+                catch (Exception ex) {
+                    Toast.makeText(getActivity(), "Error!", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -94,10 +91,7 @@ public class FragmentCRUD extends Fragment {
             @Override
             public void onClick(View view) {
                 String input = editTextBossFullName.getText().toString();
-                output = (ArrayList<String>) listOfBosses
-                        .stream()
-                        .filter(value -> value.startsWith(input))
-                        .collect(Collectors.toList());
+                output = (ArrayList<String>) listOfBosses.stream().filter(value -> value.startsWith(input)).collect(Collectors.toList());
                 Intent intent = new Intent(getActivity(), ResultActivity.class);
                 (getActivity()).startActivity(intent);
             }
@@ -109,20 +103,18 @@ public class FragmentCRUD extends Fragment {
                 try {
                     if (!selectedItemValue.isEmpty()) {
                         if (!listOfBosses.contains(editTextBossFullName.getText().toString())) {
-                            listOfBosses.set(selectedItemPosition, editTextBossFullName
-                                    .getText().toString());
+                            listOfBosses.set(selectedItemPosition, editTextBossFullName.getText().toString());
                             adapter.notifyDataSetChanged();
                             listViewOfBosses.clearChoices();
                         }
                         else {
-                            Toast.makeText(getActivity(), "There is already an element with this name.",
-                                    Toast.LENGTH_LONG).show();
+                            Toast.makeText(getActivity(), "There is already an element with this name.", Toast.LENGTH_LONG).show();
                         }
                     }
 
-                } catch (Exception ex) {
-                    Toast.makeText(getActivity(), "Select an item first.",
-                            Toast.LENGTH_LONG).show();
+                }
+                catch (Exception ex) {
+                    Toast.makeText(getActivity(), "Select an item first.", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -134,13 +126,29 @@ public class FragmentCRUD extends Fragment {
                     listOfBosses.remove(selectedItemPosition);
                     adapter.notifyDataSetChanged();
                     listViewOfBosses.clearChoices();
-                } catch (Exception ex) {
-                    Toast.makeText(getActivity(), "Error!",
-                            Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception ex) {
+                    Toast.makeText(getActivity(), "Error!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        return view;
+        frameLayout.addView(view);
+
+        return frameLayout;
     }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        frameLayout.removeAllViews();
+
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        view = inflater.inflate(R.layout.fragment_crud, null);
+
+        frameLayout.addView(view);
+    }
+
 }
--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.lab1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-/*    Button buttonSelectAll;
-    Button buttonDeselectAll;
-    Button buttonPrintAll;*/
+    FragmentCRUD fragmentCRUD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentCRUD fragmentCRUD = new FragmentCRUD();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
-                .beginTransaction();
+        fragmentCRUD = new FragmentCRUD();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.FragmentContainer, fragmentCRUD);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putStringArrayList("listOfBosses", fragmentCRUD.listOfBosses);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        fragmentCRUD.listOfBosses = savedInstanceState.getStringArrayList("listOfBosses");
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
